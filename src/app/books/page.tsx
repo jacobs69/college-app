d9534a6107578b56229049e63efe794989f1c31d
jai <jaikantharia@gmail.com>
2025-07-16 19:22:19 +0530

    added use client

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,1 +1,5 @@
-"use client"; 
+"use client";
+
+export default function BooksPage() {
+  return <div style={{ color: '#283452', fontSize: 24, textAlign: 'center', marginTop: 40 }}>Books Page</div>;
+} 
